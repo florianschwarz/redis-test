51b5d36dad5c6925090fca25be51a9c9069668b9
Florian Schwarz <fs@tresmo.de>
2014-08-11 14:34:47 +0200

    added test framework

---
+++ test/redis-tests.js
@@ -0,0 +1,26 @@
+var assert = require('chai').assert;
+
+describe('Redis tests', function () {
+
+    before(function (done) {
+        done();
+    });
+
+
+    after(function (done) {
+        done();
+    });
+
+    beforeEach(function (done) {
+        done();
+    });
+
+    afterEach(function (done) {
+        done();
+    });
+
+    it('First mocha test', function (done) {
+        assert.ok(true);
+        done();
+    });
+});
