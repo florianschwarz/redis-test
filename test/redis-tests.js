e8bf34d2770e5737136ce0b10fadd83d3a5656e7
Florian Schwarz <fs@tresmo.de>
2014-08-11 14:41:36 +0200

    first redis test

--- test/redis-tests.js
+++ test/redis-tests.js
@@ -1,11 +1,14 @@
 var assert = require('chai').assert;
+var redis = require('redis');
 
 describe('Redis tests', function () {
 
+    var client;
+
     before(function (done) {
+        client = redis.createClient();
         done();
     });
-
 
     after(function (done) {
         done();
@@ -19,8 +22,13 @@ describe('Redis tests', function () {
         done();
     });
 
-    it('First mocha test', function (done) {
+
+    it('Set simple key', function (done) {
         assert.ok(true);
-        done();
+        client.set('string key', 'string val', function (err, reply) {
+            assert.notOk(err);
+            assert.equal(reply, 'OK');
+            done();
+        });
     });
 });
